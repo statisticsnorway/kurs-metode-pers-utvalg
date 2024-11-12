ddab079cc1c1293264c36e71a58c05fb1b3a4aa3
Magnar Lillegård <mld@ssb.no>
2024-11-12 10:13:03 +0100

    Testing

--- vekting_1.R
+++ vekting_1.R
@@ -1,5 +1,5 @@
 setwd("/ssb/bruker/mld/kurs-metode-pers-utvalg")
-renv::init()
+#renv::init()
 #renv::install("sampling")
 #renv::install("statisticsnorway/ReGenesees")
 library(sampling)
@@ -32,6 +32,13 @@ utvalg <- getdata(U, s)
 # Designvekten
 utvalg$d_vekt <- 1 / utvalg$Prob
 head(utvalg)
+
+################################################
+# Vi leser inn en tidligere trukket utvalgsfil #
+# slik at alle jobber med samme data.          #
+################################################
+load("utvalg.RData")
+head(utvalg)
 # Estimerer populasjonstotal (i millioner) med Horvitz-Thomsen-estimatoren
 HTestimator(utvalg$y1, utvalg$Prob) / 1e+6
 # Den faktiske totalen er:
@@ -40,19 +47,12 @@ sum(U$y1) / 1e+6
 HTestimator(utvalg$y2, utvalg$Prob) / 1e+6
 sum(U$y2) / 1e+6
 
-##########################
-# Med frafall i utvalget #
-##########################
-# Antar at responssannsynligheten (rs) kun er avhengig av utdanning;
-# jo høyere utdanning, desto høyere sannsynlighet for å svare
-# på undersøkelsen.
-utvalg$rs <- ifelse(utvalg$utd == "1", 0.30,
-                    ifelse(utvalg$utd == "2", 0.40,
-                           ifelse(utvalg$utd == "3", 0.50, 0.60)))
-# Nettoutvalget
-netto <- utvalg[runif(nrow(utvalg)) < utvalg$rs, ]
-table(netto$kjonn, netto$ald_grp)
-
+#########################################################
+# Med frafall: Vi leser inn en tidligere laget nettofil #
+# # slik at alle jobber med samme data.                 #
+#########################################################
+load("netto.RData")
+head(netto)
 # Vi justerer designvektene basert på antagelse om helt tilfeldig frafall
 # "Missing Complete at Random" (MCAR)
 netto$mcar_vekt <- netto$d_vekt * nrow(utvalg) / nrow(netto)
@@ -61,6 +61,25 @@ crossprod(netto$mcar_vekt, netto$y1) / 1e+6
 sum(U$y1) / 1e+6
 #
 crossprod(netto$mcar_vekt, netto$y2) / 1e+6
+sum(U$y2) / 1e+6
+#
+row.names(netto) <- NULL
+save(netto, file = "netto.RData" )
+
+##################################
+# Etterstratifisering mot region #
+##################################
+t_U <- table(U$region)
+t_n <- table(netto$region)
+w <- t_U / t_n
+w
+netto$es_vekt <- w[netto$region]
+#
+# Estimering av populasjonstotaler (vektet sum)
+crossprod(netto$es_vekt, netto$y1) / 1e+6
+sum(U$y1) / 1e+6
+#
+crossprod(netto$es_vekt, netto$y2) / 1e+6
 sum(U$y2) / 1e+6
 
 ################################################
@@ -80,22 +99,6 @@ sum(U$y1) / 1e+6
 crossprod(netto$es_vekt, netto$y2)
 sum(U$y2) / 1e+6
 
-##################################
-# Etterstratifisering mot region #
-##################################
-t_U <- table(U$region)
-t_n <- table(netto$region)
-w <- t_U / t_n
-w
-netto$es_vekt <- w[netto$region]
-#
-# Estimering av populasjonstotaler (vektet sum)
-crossprod(netto$es_vekt, netto$y1) / 1e+6
-sum(U$y1) / 1e+6
-#
-crossprod(netto$es_vekt, netto$y2) / 1e+6
-sum(U$y2) / 1e+6
-
 ############################################################################
 # Kalibrering med ReGenesees. Kalibreringsmodell: region + kjonn * ald_grp #
 ############################################################################
@@ -103,34 +106,64 @@ des <- e.svydesign(data = netto, ids = ~pnr, weight = ~d_vekt)
 tot_temp <- pop.template(data = des,
                          calmodel = ~region + kjonn * ald_grp - 1)
 tot_temp
-tot_pop <- fill.template(universe = U, temp = tot_temp)
+tot_pop <- fill.template(universe = U,
+                         temp = tot_temp)
 tot_pop
-descal <- e.calibrate(design = des, df.population = tot_pop, calfun = "linear")
+descal <- e.calibrate(design = des,
+                      df.population = tot_pop,
+                      calfun = "linear")
 netto$kal_vekt <- weights(descal)
 head(netto)
 #
 summary(netto$d_vekt / mean(netto$d_vekt) * mean(netto$kal_vekt) / netto$kal_vekt)
-#
-svystatTM(descal, y = ~y1, vartype = "se") / 1e+6
+##################################
+svystatTM(descal, 
+          y = ~y1, 
+          vartype = "se") / 1e+6
+
 sum(U$y1) / 1e+6
-#
-svystatTM(descal, y = ~y2, vartype = "se") / 1e+6
+######################################
+svystatTM(descal, 
+          y = ~y2, 
+          vartype = "se") / 1e+6
+
 sum(U$y2) / 1e+6
-#
-svystatTM(descal, y = ~y1, by = ~region, vartype = "se")
+######################################
+svystatTM(descal, 
+          y = ~y1, 
+          by = ~region, 
+          vartype = "se")
+
 tapply(U$y1, U$reg, sum)
-#
-svystatTM(descal, y = ~y1, by = ~kjonn * ald_grp, vartype = "se")
+#######################################
+svystatTM(descal, 
+          y = ~y1, 
+          by = ~kjonn * ald_grp, 
+          vartype = "se")
+
 tapply(U$y1, list(U$kjonn, U$ald_grp), sum)
-#
-svystatTM(descal, y = ~y1, by = ~utd, estimator = "Mean", vartype = "se")
+#############################################
+svystatTM(descal, 
+          y = ~y1, 
+          by = ~utd, 
+          estimator = "Mean", 
+          vartype = "se")
+
 tapply(U$y1, U$utd, mean)
-#
-svystatL(descal, expression(y1 / y2), vartype = "se")
+############################################
+svystatL(descal, 
+         expression(y1 / y2), 
+         vartype = "se")
+
 sum(U$y1) / sum(U$y2)
+############################################
+svystatL(descal, 
+         expression(y1 / y2), 
+         by = ~region, 
+         vartype = "se", 
+         conf.int = TRUE)
 
-svystatL(descal, expression(y1 / y2), by = ~region, vartype = "se", conf.int = TRUE)
 tapply(U$y1, U$region, sum) / tapply(U$y2, U$region, sum)
-
+##########################################################
 
   
